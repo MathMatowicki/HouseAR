3a6ead4d09ad109794d56b375a75ed7fc9bed4f9
MathMatowicki <matowicki@gmail.com>
2023-11-20 14:20:57 +0100

    Axios added and first get working

--- front/app/(tabs)/index.tsx
+++ front/app/(tabs)/index.tsx
@@ -1,13 +1,44 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import axios, { AxiosResponse } from "axios";
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import EditScreenInfo from "../../components/EditScreenInfo";
+import { Text, View } from "../../components/Themed";
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
 
 export default function TabOneScreen() {
+  const [data, setData] = useState<WeatherForecast[]>([]);
+
+  useEffect(() => {
+    // Replace 'https://localhost:5001/api/weatherforecast' with your Minimal API endpoint
+    axios
+      .get<WeatherForecast[]>("http://localhost:5113/WeatherForecast")
+      .then((response: AxiosResponse<WeatherForecast[]>) => {
+        setData(response.data);
+      })
+      .catch((error: Error) => {
+        console.error(error);
+      });
+  }, []);
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text style={styles.title}>Weather Forecast:</Text>
+      {data.map((forecast, index) => (
+        <Text key={index}>{`${forecast.date}: ${forecast.summary}`}</Text>
+      ))}
+
+      <Text>{data.every((d) => d.date)}</Text>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
@@ -16,16 +47,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
